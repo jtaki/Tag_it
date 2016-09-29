389527cefe3bb7282f3b95b073a5c21afdb31a57
Jeff Takiguchi <jefftakiguchi@gmail.com>
2016-09-28 18:08:18 -1000

    added custombach maker

---
+++ custom/custom.rb
@@ -0,0 +1,57 @@
+#!usr/bin/env ruby
+
+require 'roo'
+require 'roo-xls'
+require './lib/item'
+require './lib/tagit/item_helper'
+
+
+include Helper
+# Load array of item numbers to upload.
+importsheet = Roo::Spreadsheet.open('data/vplbl3h9bent.xls')
+
+imports = []
+2.upto(importsheet.last_row) do |line|
+  item = {
+    num: importsheet.cell("A", line).to_i,
+    desc: clean_names(importsheet.cell("F", line))
+  }
+  imports << item
+  # item = []
+  #   item << importsheet.cell("A", line).to_i
+  # imports << item
+end
+
+# return array of hash values for num symbol
+imports_numbers = []
+imports.each do |x|
+  n = x.values_at(:num)
+  imports_numbers << n
+end
+
+existing = Roo::Spreadsheet.open('custom/logs/custom.xlsx')
+array = []
+2.upto(existing.last_row) do |line|
+  item = {
+    num: existing.cell("A", line).to_i
+  }
+    array << item
+end
+
+existing_numbers = []
+array.each do |x|
+  n = x.values_at(:num)
+  existing_numbers << n
+end
+
+unnamed_nums = imports_numbers - existing_numbers
+
+# # # for each unnamed num, match item info back in from importsheet
+exports = imports.select { |h| unnamed_nums.include?(h.values_at(:num)) }
+# push all unnamed numbers & names to cs
+
+CSV.open('rename_these.csv', "w", :row_sep => :auto) do |csv|
+  exports.each do |e|
+    csv << [e.values_at(:num), e.values_at(:desc)].flatten
+  end
+end
