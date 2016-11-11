d97dd95f043fbd04e3842e56fac2a53fec36351c
Jeff Takiguchi <jefftakiguchi@gmail.com>
2016-11-11 10:13:53 -1000

    updated gitignore

--- spec/compar_spec.rb
+++ spec/compar_spec.rb
@@ -5,7 +5,21 @@ describe Comparative do
   # before(:each) do
   #   Item.reset_all
   # end
-  let(:item) { Item.new( )}
+  let(:item) { Item.new(
+      num: 1234567,
+      nam: "test product 1",
+      wei: "3.25#",
+      pac: "60",
+      pri: 3.45,
+      cw: "N",
+      rw: "N",
+      brand: "some brand",
+      upc: "0987654321",
+      vin: "12345",
+      sym: "&",
+      vid: "12345"
+
+    )}
 
   it "gets the comparative unit" do
     expect(get_comparative_units(item)).to eq("LB")
