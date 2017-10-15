0b204558142d05315a61e670e44cfe7cfefadf24
Jeff Takiguchi <jefftakiguchi@gmail.com>
2017-10-14 21:07:28 -1000

    tests passing for name database

--- lib/name_database.rb
+++ lib/name_database.rb
@@ -16,27 +16,27 @@ class NameDatabase
     @items = @store.transaction do
       @store.fetch('data', "key does not exist")
     end
-    puts @items
   end
 
-  def find_by_number(number)
-    key = number.to_sym
+  def find_by_number(number) # returns item struct
+    key = number
+    @items.fetch(number, "item does not exist")
   end
   # accepts an array of arrays
   # needs to append to load_data
-  def append_new(arr)
+  def append_new_item(arr)
     item = Item.new(arr[0], arr[1], arr[2])
     if check_duplicate?(item.number)
-      update_database(item)
+      update_database(item, 'item.number')
     end
   end
 
   private
 
-  def update_database(item)
+  def update_database(item, keyname)
     @store.transaction do
       # unless the key (itemnum) already exists, then store it
-      @store['data'][item.number] = item
+      @store['data'][keyname] = item
       @store.commit
     end
   end
--- spec/name_database_spec.rb
+++ spec/name_database_spec.rb
@@ -4,24 +4,34 @@ describe NameDatabase do
   let(:db) { NameDatabase.new('test_database.yml') }
   let(:name_array) { [11111,"test", "test"] }
 
-  it 'can persist information into the yml file' do
-    db.append_new(name_array)
+  context 'information reduction' do
 
-    expect()
+    it 'can persist information into the yml file' do
+      db.append_new_item(name_array)
+      db.load_data
+
+      expect(db.items).to include(name_array[0])
+    end
+
+    it 'can retrieve information from the yml file' do
+      db.load_data
+
+      expect(db.items.has_key?(12345)).to eq(true)
+    end
+
   end
 
-  xit 'can retrieve specific name by number' do
+  it 'can retrieve specific name by number' do
+    db.load_data
+    num = name_array[0]
+    item_lookup = db.find_by_number(num)
+
+    expect(item_lookup[:number]).to eq(num)
   end
 
   it 'creates a new item struct' do
     item = NameDatabase::Item.new(1111,"test","test")
     expect(item).to be_a(Struct)
-  end
-
-  it 'can retrieve information from the yml file' do
-    db.load_data
-
-    expect(db.items.has_key?(12345)).to eq(true)
   end
 
 end
